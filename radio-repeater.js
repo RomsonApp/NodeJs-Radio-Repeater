6d5a75967740df26b0ccf2ee37dedff2df48a383
Vladislav Cherepenya <romsonapp@gmail.com>
2015-10-24 22:51:12 +0300

    init

---
+++ radio-repeater.js
@@ -0,0 +1,22 @@
+var http = require('http'),
+	fs = require('fs'),
+	radioServer = 'http://195.95.206.20/HitFM',
+	port = '8080';
+
+http.createServer(handler).listen(port);
+
+console.log('Server is created');
+
+function handler(req, res) {
+	res.writeHead(200, {
+		'Content-Type':'audio/mpeg',
+		'Access-Control-Allow-Origin': 'http://localhost'
+	});
+	
+	http.get(radioServer, function(response){
+		console.log(response.clients);
+		response.on('data', function (data) {
+			res.write(data);
+		});
+	});
+}
